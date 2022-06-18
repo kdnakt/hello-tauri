87ffe0e413a916bdc0ce1dea62053bc736d076f8
kdnakt <a.kid.1985@gmail.com>
2022-06-18 23:33:55 +0900

    call greet api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api'
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
 
 function App() {
   return (
